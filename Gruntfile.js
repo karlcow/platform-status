81d7511d8d5582ad036ccfd41c48b2b72f8dc4cc
Anthony Ricaud <anthony@ricaud.me>
2015-07-17 19:30:02 +0200

    Task to convert the JSON back to our desired format

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+
+  grunt.registerTask('convert_features_json', function() {
+    var categories = grunt.file.readJSON('features.json');
+
+    var converted_features = [];
+    for (var category_name in categories) {
+      var features = categories[category_name];
+      for(var i in features) {
+        features[i].category = category_name;
+        converted_features.push(features[i]);
+      }
+    }
+
+    grunt.file.write('converted_features.json', JSON.stringify(converted_features));
+  });
+};
