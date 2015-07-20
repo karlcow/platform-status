f6d8b5ac7281a3aa317541a16013f6ea56d66425
Anthony Ricaud <anthony@ricaud.me>
2015-07-20 19:43:24 +0200

    Add filtering

--- modules/App.js
+++ modules/App.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var PureRenderMixin = require('react/addons').addons.PureRenderMixin;
 var {Link, RouteHandler} = require('react-router');
 
 require('./App.css');
@@ -6,15 +7,35 @@ require('./App.css');
 var FEATURES = require('./converted_features.json');
 
 const FilterableFeatureTable = React.createClass({
+  getInitialState() {
+    return {
+      filterText: ''
+    }
+  },
+
+  handleSearchInput(filterText) {
+    this.setState({
+      filterText
+    })
+  },
+
   render() {
     const categories = [...new Set(this.props.features.map((feature) => (feature.category)))];
     const statuses = [...new Set(this.props.features.map((feature) => (feature.status)))];
 
+    const filterText = this.state.filterText.toLowerCase();
+    const features = this.props.features.filter((feature) => {
+      return feature.name.toLowerCase().includes(filterText);
+    });
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar
+          filterText={this.state.filterText}
+          onUserInput={this.handleSearchInput}
+        />
         <div className="foo">
-          <FeatureTable features={this.props.features} />
+          <FeatureTable features={features} />
           <div>
             <FilterBox name="Categories" list={categories} />
             <FilterBox name="Status" list={statuses} />
@@ -26,15 +47,25 @@ const FilterableFeatureTable = React.createClass({
 });
 
 const SearchBar = React.createClass({
-  render() {
+  handleChange() {
+    this.props.onUserInput(this.refs.filterTextInput.getDOMNode().value);
+  },
 
+  render() {
     return (
-      <input placeholder="filter…"/>
+      <input
+        placeholder="filter…"
+        value={this.props.filterText}
+        ref="filterTextInput"
+        onChange={this.handleChange}
+      />
     );
   }
 });
 
 const FilterBox = React.createClass({
+  mixins: [PureRenderMixin],
+
   render() {
     const rows = this.props.list.map((value) => (
       <li key={value}>
@@ -55,6 +86,8 @@ const FilterBox = React.createClass({
 });
 
 const FeatureTable = React.createClass({
+  mixins: [PureRenderMixin],
+
   render() {
     var rows = this.props.features.map((feature) => {
       return (<FeatureRow key={feature.name} feature={feature} />)
@@ -68,6 +101,8 @@ const FeatureTable = React.createClass({
 });
 
 const FeatureRow = React.createClass({
+  mixins: [PureRenderMixin],
+
   render() {
     return (
       <li>{this.props.feature.name}</li>
